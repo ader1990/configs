3f40fb6372dfc628a61b66b3f411ffcaff746d02
Adrian Vladu <avladu@cloudbasesolutions.com>
2014-03-24 23:22:13 +0100

    Added grizzly support

--- create-networks-openstack.sh
+++ create-networks-openstack.sh
@@ -1,14 +1,17 @@
-start=$1
-end=$2
+FROM_IP=$1
+TO_IP=$2
+neutron_cmd="neutron"
+if [ -n $3 ]; then
+    neutron_cmd=$3
+fi
+NETID1=`$neutron_cmd net-create private --provider:network_type flat --provider:physical_network physnet1 | awk '{if (NR == 6) {print $4}}'`
+SUBNETID1=`$neutron_cmd subnet-create private 10.0.1.0/24 --dns_nameservers list=true 8.8.8.8 | awk '{if (NR == 11) {print $4}}'`
 
-NETID1=`neutron net-create private --provider:network_type flat --provider:physical_network physnet1 | awk '{if (NR == 6) {print $4}}'`
-SUBNETID1=`neutron subnet-create private 10.0.1.0/24 --dns_nameservers list=true 8.8.8.8 | awk '{if (NR == 11) {print $4}}'`
+ROUTERID1=`$neutron_cmd router-create router | awk '{if (NR == 7) {print $4}}'`
 
-ROUTERID1=`neutron router-create router | awk '{if (NR == 7) {print $4}}'`
+$neutron_cmd router-interface-add $ROUTERID1 $SUBNETID1
 
-neutron router-interface-add $ROUTERID1 $SUBNETID1
+EXTNETID1=`$neutron_cmd net-create public --router:external=True | awk '{if (NR == 6) {print $4}}'`
+$neutron_cmd subnet-create public --allocation-pool start=$FROM_IP,end=$TO_IP --gateway 10.7.1.1 10.7.0.0/16 --enable_dhcp=False
 
-EXTNETID1=`neutron net-create public --router:external=True | awk '{if (NR == 6) {print $4}}'`
-neutron subnet-create public --allocation-pool start=$start,end=$end --gateway 10.7.1.1 10.7.0.0/16 --enable_dhcp=False
-
-neutron router-gateway-set $ROUTERID1 $EXTNETID1
+$neutron_cmd router-gateway-set $ROUTERID1 $EXTNETID1

0630230493be179d8762314b143b46fdd99df9c8
Adrian Vladu <avladu@cloudbasesolutions.com>
2014-07-30 23:34:03 +0300

    juno update

--- create-networks-openstack.sh
+++ create-networks-openstack.sh
@@ -14,7 +14,7 @@ fi
 NETID1=`$neutron_cmd net-create private --provider:network_type flat --provider:physical_network physnet1 | awk '{if (NR == 6) {print $4}}'`
 SUBNETID1=`$neutron_cmd subnet-create private 10.0.1.0/24 --dns_nameservers list=true 8.8.8.8 | awk '{if (NR == 11) {print $4}}'`
 
-ROUTERID1=`$neutron_cmd router-create router | awk '{if (NR == 7) {print $4}}'`
+ROUTERID1=`$neutron_cmd router-create router | awk '{if (NR == 8) {print $4}}'`
 
 $neutron_cmd router-interface-add $ROUTERID1 $SUBNETID1
 

afbf9731408ab757e51e6b4987f87aaf0409a470
Adrian Vladu <avladu@cloudbasesolutions.com>
2016-01-03 07:01:43 -0500

    Added script and how-to for wi-fi hotspot on Windows

---
+++ how-to-create-wifi-hotspot-windows.ps1
@@ -0,0 +1,18 @@
+#ps1
+
+# How to set up:
+# http://lifehacker.com/turn-your-windows-10-computer-into-a-wi-fi-hotspot-1724762931
+ 
+# How to debug:
+# http://stackoverflow.com/questions/18182084/cant-start-hostednetwork
+
+$password = "myverystr@ngp@ssw0rd"
+$SSID = "MyAwesomeHotspot"
+
+netsh wlan set hostednetwork mode=allow ssid=$SSID key=$password
+netsh wlan start hostednetwork
+
+# if the start does not work, check in Device Manager, under Network Adapters,
+# the "Microsoft Hosted Network Virtual Adapter" to be enabled.
+# To make sure this adapter is shown, in Device Manager/View, click on
+# "Show hidden devices"
